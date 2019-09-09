09b3a28ee008e17f428dc2b0d819a18cfc86c0a7
Андрей М <librandron@gmail.com>
2019-09-09 20:12:17 +0300

    WORK DINAMIC VERSION

--- Server/Program.cs
+++ Server/Program.cs
@@ -27,18 +27,22 @@ namespace Server
                 var requestPathFile = pathIndex + request.Url.LocalPath;
                 var file = File.ReadAllText(requestPathFile);
 
-                if(request.Url.LocalPath.Contains("part.html"))
+                var userList = "";
+
+                if (request.Url.LocalPath.Contains("part.html"))
                 {
                     var fileJson = File.ReadAllText(@"c:\Users\Andrey\Source\Repos\librandron\Server\Server\users.json");
                     Database data = JsonConvert.DeserializeObject<Database>(fileJson);
-                    var userList = "";
+                   
                     foreach (var user in data.users)
                     {
                         userList += $@"<li>{user.name}</li>";
                     }
                 }
 
-                byte[] fileAsByte = System.Text.Encoding.UTF8.GetBytes(file);
+                var resultWithUsers = file.Replace("%participants%",userList);
+
+                byte[] fileAsByte = System.Text.Encoding.UTF8.GetBytes(resultWithUsers);
                 response.ContentLength64 = fileAsByte.Length;
                 output = response.OutputStream;
                 output.Write(fileAsByte, 0, fileAsByte.Length);
